0965c5d8fa15929a5c133189526d0df9ec821360
Alexsandr <alexnem@inbox.ru>
2021-05-29 22:21:07 +0300

    install socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
 const port = process.env.PORT || 3001;
 const publicDirPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('new web socket connection');
+
+  // socket.emit('');
+})
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 })
 
