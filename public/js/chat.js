3216954dbb488cb07696fc549ea8f7c048e633b7
Alexsandr <alexnem@inbox.ru>
2021-05-30 21:00:19 +0300

    rendering messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,34 @@
 const socket = io();
 
 // Elements
-
 const $messageForm = document.querySelector('#message-form');
 const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('button');
+const $messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
 
 socket.on('message', (message) => {
   console.log(message, 'message');
+
+  const html = Mustache.render(messageTemplate, {
+    message
+  });
+
+  console.log(html, 'html');
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
 $messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  $messageFormButton.setAttribute('disabled', 'disabled');
 
   const message = e.target.elements.message.value;
+
+  if (message === '') return;
+
+  $messageFormButton.setAttribute('disabled', 'disabled');
 
   socket.emit('sendMessage', message, (error) => {
     $messageFormButton.removeAttribute('disabled');
